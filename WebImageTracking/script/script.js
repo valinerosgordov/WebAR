59a3c6d7c0dc59ccf6e4a451a75f18f7061b0d05
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-09-26 20:20:15 +0300

    add function (JSON)

---
+++ WebImageTracking/script/script.js
@@ -0,0 +1,24 @@
+let arrayUsers = []
+const inputName = document.getElementById('userInput')
+
+const redirectWebSlam = async () => {
+    await fetchData();
+    nextPage()
+}
+
+const nextPage = () => {
+    const name = inputName.value
+    const idxName = arrayUsers.findIndex( i => i.name === name)
+    const url = `https://konti.digitalmarker.ru/slam?${idxName}`
+    nextBtn.href = url
+}
+
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://arplatov-6e62e-default-rtdb.firebaseio.com/baseNames.json')
+        const {Names} = await response.json();
+        arrayUsers = Names            
+    } catch (error) {
+        console.error('Error fetching Names:', error);
+    }
+}
---
+++ WebSlam/script/script.js
@@ -0,0 +1,40 @@
+let soundname= undefined;
+        
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://arplatov-6e62e-default-rtdb.firebaseio.com/baseNames.json')
+        const {Names} = await response.json();
+        
+        console.log(Names); 
+        
+        const url = window.location.href
+        const newurl = url.split("?")
+        const idx = newurl[newurl.length-1]
+        soundname = Names[idx]?.sound
+
+        console.log(soundname)
+
+        playSound()
+        
+    } catch (error) {
+        console.error('Error fetching Names:', error);
+    }
+}
+
+const playSound = () =>{
+    let audioStart = new Audio(`/slam/firstPart.wav`);
+    let audioName = new Audio(`/slam/audio/speech/${soundname}`);
+    let audioFinaly = new Audio(`/slam/secondPart.wav`);
+
+    audioStart.onloadedmetadata = () => {
+        let durationStart = (audioStart.duration*1000) + 500
+        audioStart.play(); 
+        setTimeout(() => {
+            let durationName = (audioName.duration*1000) + 200
+            audioName.play(); 
+            setTimeout(() => {
+                audioFinaly.play(); 
+            }, durationName)  
+        }, durationStart)                 
+    }
+}
