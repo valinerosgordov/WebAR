b50cfe0cf0c960877c86c11e18c5d752faa56618
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-09-26 20:39:01 +0300

    fix

--- WebImageTracking/script/script.js
+++ WebImageTracking/script/script.js
@@ -1,6 +1,6 @@
 const redirectWebSlam = async (userName) => {
     const nextBtn = document.querySelector('.nextBtn')
-    
+
     try {
         const response = await fetch('https://arplatov-6e62e-default-rtdb.firebaseio.com/baseNames.json')
         const { Names } = await response.json();
@@ -9,8 +9,9 @@ const redirectWebSlam = async (userName) => {
         const idxName = arrayUsers.findIndex( i => i.name === userName)
         const url = `https://konti.digitalmarker.ru/slam?${idxName}`
         nextBtn.href = url
+        window.location.href = url
 
     } catch (error) {
-        console.error('Error fetching Names:', error);
+        console.error('Error fetching Names:', error)
     }
 }
